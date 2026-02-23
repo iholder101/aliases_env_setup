cb9e127fa988d9157b8cb2e4f87cfe3ecadd8f22
Itamar Holder <iholder@redhat.com>
2026-02-23 08:19:15 +0200

    fix spl-tmux: 3 panes, bottom 25% of the height
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- aliases.sh
+++ aliases.sh
@@ -32,7 +32,7 @@ alias mkc='make clean; make'
 alias scale-text='function temp_func { gsettings set org.gnome.desktop.interface text-scaling-factor $1; } ; temp_func'
 alias cls='cl; s'
 alias claude-tmux='/home/iholder/tmux_with_claude.sh'
-alias spl-tmux='tmux new-session \; split-window -h -b \; select-pane -t 0 \; select-pane -t 2'
+alias spl-tmux='tmux new-session \; split-window -v -p 25 \; select-pane -U \; split-window -h \; select-pane -t 0'
 alias set-brighness='function temp_func { sudo ddcutil setvcp 10 "$1"; } ; temp_func'
 
 #Sed patterns
